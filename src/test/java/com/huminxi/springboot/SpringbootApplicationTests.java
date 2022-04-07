b61b63ca196353631b362417fe77e967e84edfb1
huminxicn <huminxi2017@gmail.com>
2022-04-07 19:37:12 +0800

    springboot相关的代码

---
+++ src/test/java/com/huminxi/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huminxi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
